f35ddad71810d65c5065a8a7fca5f29da8192b49
Joe Dempsey <jdemps22@students.kennesaw.edu>
2019-03-20 01:15:31 -0400

    Added commands "echo" and "pick" (located in Misc.cs) . These are just temporary commands, not intended for use in the final product.

---
+++ DiscordDnDBot/Modules/Misc.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using System.Threading.Tasks;
+
+namespace DiscordDnDBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echoed message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+            
+            await Context.Channel.SendMessageAsync("",false, embed.Build());
+        }
+
+        [Command("pick")]
+        public async Task Pick([Remainder]string message)
+        {
+            string[] options = message.Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            Random rand = new Random();
+            string selection = options[rand.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Choice for " + Context.User.Username);
+            embed.WithDescription(selection);
+            embed.WithColor(new Color(65, 255, 100));
+            embed.WithThumbnailUrl("https://media1.tenor.com/images/5c406b927ec59a31eb67e3366f3121ef/tenor.gif?itemid=11909469");
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+    }
+}

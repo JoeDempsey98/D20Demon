7b580e603b2d815dc0bec8b535c11000c5b17ec2
Joe Dempsey <jdemps22@students.kennesaw.edu>
2019-03-19 23:45:56 -0400

    Add project files.

---
+++ DiscordDnDBot/Config.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace DiscordDnDBot
+{
+    class Config
+    {
+        private const string configFolder = "Resources";
+        private const string configFile = "config.json";
+
+        public static BotConfig bot;
+
+        static Config()
+        {
+            if (!Directory.Exists(configFolder))
+                Directory.CreateDirectory(configFolder);
+
+            if (!File.Exists(configFolder + "/" + configFile))
+            {
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(configFolder + "/" + configFile, json);
+            }
+            else
+            {
+                string json = File.ReadAllText(configFolder + "/" + configFile);
+                bot = JsonConvert.DeserializeObject<BotConfig>(json);
+            }
+        }
+    }
+
+    public struct BotConfig
+    {
+        public string token;
+        public string cmdPrefix;
+    }
+}

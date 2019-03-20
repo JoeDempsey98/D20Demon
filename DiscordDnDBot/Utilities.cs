2bc9d555392074af17ca7ac8a662dd435c9db5b7
Joe Dempsey <jdemps22@students.kennesaw.edu>
2019-03-20 01:49:51 -0400

    Added more functionality to Utilities.cs.

--- DiscordDnDBot/Utilities.cs
+++ DiscordDnDBot/Utilities.cs
@@ -25,5 +25,18 @@ namespace DiscordDnDBot
             return "";
         }
 
+        public static string GetFormattedAlert(string key, object parameter)
+        {
+            return GetFormattedAlert(key, new object[] { parameter });
+        }
+
+        public static string GetFormattedAlert(string key, params object[] parameter)
+        {
+            if (alerts.ContainsKey(key))
+            {
+                return String.Format(alerts[key], parameter);
+            }
+            return "";
+        }
     }
 }

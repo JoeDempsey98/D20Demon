b88d8e4004137304368f06b8e2d676ff2ab4e0ea
Joe Dempsey <jdemps22@students.kennesaw.edu>
2019-03-26 19:49:39 -0400

    Added the framework for image generation.

---
+++ DiscordDnDBot/Core/LevelingSystem/UserLevelDisplayImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DiscordDnDBot.Core.LevelingSystem
+{
+    internal class UserLevelDisplayImage
+    {
+        private string directory;
+        private string file;
+        
+        public void HtmlToJpeg(string html)
+        {
+            //generate an image from html and store it in directory/file
+        }
+    }
+}
